76d444ec29832ec1bf5c5d2a80b4a444b0c0ecda
Tina Ly <t.ly@mail.utoronto.ca>
2025-09-10 17:08:33 -0400

    Fixed multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -9,7 +9,7 @@ public class Multiples {
         int counter = 0;
         for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
-                counter += 1;
+                counter += i;
             }
         }
         return counter;
